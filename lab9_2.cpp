6172530da812c1376d761fa1193f85c88f7d98a6
phurin2644 <phus.2644@gmail.com>
2021-01-08 17:20:33 +0700

    lab9_2com

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -6,11 +6,17 @@ using namespace std;
 int main (){
 	ifstream source;
 	ofstream dest;
-	source.open("cheerbook.txt") ;
+	source.open("cheerbook.txt");
 	dest.open("cheerbook_copy.txt");
+	string textline;
+	cout << "-------------------- BOOM ---------------------" << endl;
+	while (getline(source,textline)){
+		cout << textline << "\n";
+	}
+	cout << "-------------------- HA!! ---------------------";
+;
 	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
+
 
     source.close();
     dest.close();
