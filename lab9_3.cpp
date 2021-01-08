addd14f0d50d25751a9fc39aaf4e9797ee78c4f7
phurin2644 <phus.2644@gmail.com>
2021-01-08 18:00:42 +0700

    lab9_com

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,26 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+    ifstream source;
+	source.open("score.txt");
+	int count = 0;
+	float sum = 0;
+	float sum2 = 0;
+	string textline;
+	while (getline(source,textline)){
+		sum += atof(textline.c_str());
+		sum2 += pow(atof(textline.c_str()),2);
+		count++;
+	}
+
+    cout << "Number of data = " << count << endl;
+    
+    cout << setprecision(3);
+    cout << "Mean = " << sum/count << endl;
+    cout << "Standard deviation = " << sqrt((sum2/count)-pow((sum/count),2));
+}
